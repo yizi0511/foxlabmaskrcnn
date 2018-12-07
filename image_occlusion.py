732913c7ddff376b1a7f4215766d3762a9ee21de
yizi <yizzhang@ucdavis.edu>
2018-12-07 10:36:20 -0800

    clean uo

--- image_occlusion.py
+++ image_occlusion.py
@@ -9,25 +9,25 @@ from glob import glob
 
 def Vmask(im, y):
     vmask = im.copy()
-    vmask[:, np.sin(y)<0,:] = 0
+    vmask[:, np.sin(y) < -.9, 0:3] = 0
     return vmask
 
 def Cmask(im, x, y):
     vmask = im.copy()
-    vmask[:,np.sin(y)<0,:] = 0
+    vmask[:,np.sin(y) < -.9, 0:3] = 0
     
     cmask = vmask.copy()
-    cmask[np.sin(x)<0, :, :] = 0
+    cmask[np.sin(x)< -.9, :, 0:3] = 0
     return cmask
 
 def Dmask(im, x, y):
     dmask = im.copy()
     for i in x:
         for j in y:
-            if np.sin((i+j)/10) < -.7:
-                dmask[i,j,:] = 0
-            if np.sin((i-j)/10) < -.7:
-                dmask[i,j,:] = 0
+            if np.sin((i+j)/8) < -.9:
+                dmask[i,j, 0:3] = 0
+            if np.sin((i-j)/8) < -.9:
+                dmask[i,j, 0:3] = 0
     
     return dmask
 
@@ -41,11 +41,11 @@ def main():
 
             x = list(range(0, img.shape[0]))
             for i in x:
-                x[i] = x[i] / 8
+                x[i] = x[i] / 5
 
             y = list(range(0, img.shape[1]))
             for i in y:
-                y[i] = y[i] / 8
+                y[i] = y[i] / 5
             
             vmask = Vmask(img, y)
             cmask = Cmask(img, x, y)
